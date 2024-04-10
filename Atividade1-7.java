85e8d54f4e8330eefd287e2acf6187fe8c25c8bf
AbnerPilatti <166664492+AbnerPilatti@users.noreply.github.com>
2024-04-10 19:53:56 -0300

    Create Atividade1-7.java

---
+++ Atividade1-7.java
@@ -0,0 +1,42 @@
+/**
+ * Copie o código abaixo, encontre e ajuste todos os problemas que existem nele:
+ *   public class ControleEstoque {
+ *       public static void main(String[] args) {
+
+ *           String nomeProduto;
+ *           double precoProduto;
+ *           int quantidadeProduto;
+
+ *           nomeProduto = 12345;
+ *           precoProduto = "29.99";
+ *           quantidadeProduto = "10";
+
+ *           String marcaProduto;
+ *           marcaProduto = true;
+
+ *           String 1nomeProduto = "Fone de Ouvido";
+ *           double preçoProduto = 29,99;
+
+ *           System.out.println("Dados do Produto":);
+ *           System.out.println("Nome: " + nomeProduto)
+ *           System.out.println("Marca: " + marcaProduto);
+ *           System.out.println("Preço: $" + precoProduto);
+ *           System.out.println("Quantidade em estoque: " + quantidadeProduto);
+ *       }
+ *   }
+ */
+
+public class Atividade1-7 {
+    public static void main(String[] args) {
+        String nomeProduto = "Celular";
+        String marcaProduto = "Samsung";
+        double precoProduto = 3000;
+        int quantidadeProduto = 4;
+
+        System.out.println("Dados do Produto:");
+        System.out.println("Nome: " + nomeProduto);
+        System.out.println("Marca: " + marcaProduto);
+        System.out.println("Preço: $" + precoProduto);
+        System.out.println("Quantidade em estoque: " + quantidadeProduto);
+    }
+}
