78dbb4515668fff5f1f389c2f25c32ec704af2cd
AbnerPilatti <166664492+AbnerPilatti@users.noreply.github.com>
2024-04-10 19:34:00 -0300

    Update Atividade1-1.java

--- Atividade1-1.java
+++ Atividade1-1.java
@@ -1,17 +1,17 @@
 // "Crie 10 variáveis de sua escolha, utilize tipos de variáveis diferentes."
 
-class Atividade1_1 {
+class Atividade1-1.java {
     public static void main(String[] args) {  
       
-        int idade = 19;
-        double peso = 20.54;
-        float nota = 6.8f;
-        long data = 20022024;
-        short ano = 2024;
-        byte unidade = 2;       
+        int idade = 20;
+        double peso = 18.10;
+        float nota = 10.8f;
+        long data = 20032024;
+        short ano = 2020;
+        byte unidade = 4;       
         char sexo = 'M';      
         boolean aprovacao = true;       
-        int semestre = 2;        
-        double altura = 1.75;
+        int semestre = 4;        
+        double altura = 1.84;
     }
 }
