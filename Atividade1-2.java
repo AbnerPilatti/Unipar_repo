0e98ba500b9c2eac2d12093ca950cb840ff6ca14
AbnerPilatti <166664492+AbnerPilatti@users.noreply.github.com>
2024-04-10 19:27:48 -0300

    Create Atividade1-2.java

---
+++ Atividade1-2.java
@@ -0,0 +1,37 @@
+/**
+ *     "Corrija os erros de nomenclatura do padrão camelCase e crie no java."
+ *     a. 1nomeProduto
+ *     b. preçoProduto%
+ *     c. marca do produto
+ *     d. quantidade produto
+ *     e. _descricaoProduto
+ *     f. NomeProduto
+ *     g. 123_numeroProduto
+ *     h. precoDoProduto
+ *     i. estoque Disponivel
+ *     j. totaldeProdutos       
+ */
+
+public class Atividade1_2.java {
+    public static void main(String[] args) {
+        String nomeProduto;
+
+        double precoProduto;
+    
+        String marcaDoProduto;
+      
+        int quantidadeProduto;
+      
+        String descricaoProduto;
+
+        String nomeDoProduto;
+      
+        int numeroProduto;
+      
+        double precoDoProduto;
+      
+        int estoqueDisponivel;
+      
+        int totaldeProdutos;
+    }
+}
