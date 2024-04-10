98be87b7786e4e701ec424530a509c960774171f
AbnerPilatti <166664492+AbnerPilatti@users.noreply.github.com>
2024-04-10 19:52:16 -0300

    Create Atividade1-6.java

---
+++ Atividade1-6.java
@@ -0,0 +1,34 @@
+/**
+ *   Ajuste o código abaixo
+ *   public class Main {
+ *       public static void main(String[] args) {
+ 
+ *           int numeroProduto = "ABC123"; 
+ *           double precoProduto = 25; 
+ 
+ *           String descricaoProduto;
+ 
+ *           System.out.println("Descrição do Produto: " + descricaoProduto);
+ 
+ *           descricaoProduto = "Fone de ouvido Bluetooth"
+ 
+ *           System.out.println("Dados do Produto:");
+ *           System.out.println("Número: " + numroProduto);
+ *           System.out.println("Preço: $" + precoProduto);
+ *           System.out.println("Descrição: " + descricaoProduto);
+ *       }
+ *   }
+ */
+
+public class Atividade1_6 {
+    public static void main(String[] args) {
+        int numeroProduto = 46;
+        double precoProduto = 2999.90;
+        String descricaoProduto = "Celular da Samsung";
+
+        System.out.println("Dados do Produto:");
+        System.out.println("Número: " + numeroProduto);
+        System.out.println("Preço: $" + precoProduto);
+        System.out.println("Descrição: " + descricaoProduto);
+    }
+}
